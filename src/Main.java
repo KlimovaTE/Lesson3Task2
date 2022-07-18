365ab2a4bd476621df137fc0da2c2d866c5e7c7e
KlimovaTE <t_klimova@inbox.ru>
2022-07-18 16:55:57 +0300

    Encoding change

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         double height = 1.8;
         double weight = 80;
         double bmi = service.calculate(height, weight);
-        System.out.print("Индекс массы тела: ");
+        System.out.print("РРЅРґРµРєСЃ РјР°СЃСЃС‹ С‚РµР»Р°: ");
         System.out.printf("%.2f", bmi);
 
     }
